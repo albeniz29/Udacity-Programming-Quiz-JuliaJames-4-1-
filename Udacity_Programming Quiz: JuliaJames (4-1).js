7d3a02bfb797e1630190da2e5ad97e0d4a0b3f8e
albeniz29 <49162136+albeniz29@users.noreply.github.com>
2019-04-28 09:08:01 -0400

    Update Udacity_Programming Quiz: JuliaJames (4-1).js

--- Udacity_Programming Quiz: JuliaJames (4-1).js
+++ Udacity_Programming Quiz: JuliaJames (4-1).js
@@ -4,7 +4,6 @@
 Instead of "Fizzbuzz" lets used "JuliaJames"
 
  Write a while loop that:
-
     Loop through the numbers 1 to 20
     If the number is divisible by 3, print "Julia"
     If the number is divisible by 5, print "James"
