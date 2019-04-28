a7266bbbabbcf4382829d2a68116241031c0784a
albeniz29 <49162136+albeniz29@users.noreply.github.com>
2019-04-28 09:05:29 -0400

    Add files via upload

---
+++ Udacity_Programming Quiz: JuliaJames (4-1).js
@@ -0,0 +1,28 @@
+/*
+ * Programming Quiz: JuliaJames (4-1)
+"Fizzbuzz" is a famous interview question used in programming interviews.
+Instead of "Fizzbuzz" lets used "JuliaJames"
+
+ Write a while loop that:
+
+    Loop through the numbers 1 to 20
+    If the number is divisible by 3, print "Julia"
+    If the number is divisible by 5, print "James"
+    If the number is divisible by 3 and 5, print "JuliaJames"
+    If the number is not divisible by 3 or 5, print the number
+ */
+
+var x = 1;
+
+while (x <= 20) {
+    if (x % 3 === 0 && x % 5 === 0){
+        console.log("JuliaJames");
+    } else if (x % 3 === 0){
+        console.log("Julia");
+    } else if (x % 5 === 0){
+        console.log("James");
+    } else if (x % 3 !== 0 || x % 5 !== 0){
+        console.log(x);
+    }
+    x = x + 1;
+}
